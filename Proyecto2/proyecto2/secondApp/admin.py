46edcde6377fcdc7949f9457ffbd0e26512ebb01
PykeSub <gastonalvarez0023@gmail.com>
2024-10-07 13:35:30 -0400

    V2

--- Proyecto2/proyecto2/secondApp/admin.py
+++ Proyecto2/proyecto2/secondApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from secondApp.models import Employee
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'email', 'fono']
 
 # Register your models here.
+admin.site.register(Employee, EmployeeAdmin)
